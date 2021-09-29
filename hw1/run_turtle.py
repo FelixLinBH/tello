c4827bcd4105f7f2e7115dcfec08fad24b77c1f9
felixlin <>
2021-09-29 21:14:13 +0800

    u

--- hw1/run_turtle.py
+++ hw1/run_turtle.py
@@ -8,22 +8,22 @@ def main():
   t2 = simple_turtle.Turtle("turtle2")
 
   t1.state.show()
-  t2.state.show()
   t1.crtl.ser_reset()
+  
+  t2.state.show()
   t2.crtl.ser_reset()
 
   msg = Twist()
   msg.linear.x = 2.0
   msg.angular.z = 2.4
   t1.crtl.move(msg, 1.0)
+  t1.state.show()  
 
   msg = Twist()
   msg.linear.x = 2.0
   msg.angular.z = -2.4
   t2.crtl.move(msg, 1.0)
-  
   t2.state.show()  
-  t1.state.show()  
   
   t1.crtl.ser_setpen(255, 0, 0, 5, 0)
   t2.crtl.ser_setpen(0, 128, 128, 5, 0)
