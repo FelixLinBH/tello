adc6cda5ab7d82e6679a0b6db8058ac0c321531f
felixlin <>
2021-09-29 20:47:24 +0800

    u

--- hw1/turtlesim_pub.py
+++ hw1/turtlesim_pub.py
@@ -17,6 +17,18 @@ from geometry_msgs.msg import Twist
 
 # # draw_star(100, "purple")
 
+def turtle_callSer():
+  rospy.wait_for_service('/reset')
+  try:
+    call1 = rospy.ServiceProxy('/reset', EmptySrv)
+    resp1 = call1()
+    return resp1
+  except rospy.ServiceException, e:
+    print "Service call failed: %s" % e
+  
+  rospy.spin()
+  
+
 def turtle_pub():
   rate = rospy.Rate(10)
   pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
@@ -24,7 +36,7 @@ def turtle_pub():
   while not rospy.is_shutdown():
     msg = Twist()
     msg.linear.x = 10
-    msg.angular.y = 60
+    msg.angular.z = 60
     rospy.loginfo(msg)
     pub.publish(msg)
     rate.sleep()
@@ -39,6 +51,7 @@ def turtle_pub():
 if __name__ == '__main__':
   rospy.init_node('turtlesim_pub', anonymous = True)
   try:
+    turtle_callSer()
     turtle_pub()
   except rospy.ROSInterruptException:
     pass
