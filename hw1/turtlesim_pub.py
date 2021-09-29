1730031e74701540983d16c51a0d54c066d6b6d3
felixlin <>
2021-09-29 20:37:06 +0800

    T

--- hw1/turtlesim_pub.py
+++ hw1/turtlesim_pub.py
@@ -2,15 +2,46 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+# def draw_star(size, color):
+#     angle = 120
+#     turtle.fillcolor(color)
+#     turtle.begin_fill()
 
+#     for side in range(5):
+#         turtle.forward(size)
+#         turtle.right(angle)
+#         turtle.forward(size)
+#         turtle.right(72 - angle)
+#     turtle.end_fill()
+#     return
+
+# # draw_star(100, "purple")
 
 def turtle_pub():
   rate = rospy.Rate(10)
   pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
+  angle = 120
   while not rospy.is_shutdown():
     msg = Twist()
-    msg.linear.x = 2.0
-    msg.angular.z = -1.8
+    msg.linear.x = 100
+    rospy.loginfo(msg)
+    pub.publish(msg)
+    rate.sleep()
+
+    msg = Twist()
+    msg.angular.z = angle
+    rospy.loginfo(msg)
+    pub.publish(msg)
+    rate.sleep()
+
+    msg = Twist()
+    msg.linear.x = 100
+    rospy.loginfo(msg)
+    pub.publish(msg)
+    rate.sleep()
+
+    msg = Twist()
+    msg.angular.z = 72 - angle
     rospy.loginfo(msg)
     pub.publish(msg)
     rate.sleep()
